9d1748f203ae5dec885523d57ac7311616f397bb
notnapat <notnapat9@gmail.com>
2023-04-07 12:49:52 +0700

    first

---
+++ react-bootstrap/src/App.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import './App.css';
+// import {Button} from "react-bootstrap"
+// import { Alert } from 'react-bootstrap';
+// import Sidebar from './Sidebar'
+
+
+
+function App() {
+  return (
+    <>
+    {/* <Sidebar/> */}
+    {/* <div className="App">
+      <h1 className='bg-danger'>aaa</h1>
+<Button variant="primary" >aaa</Button>
+    </div>
+    <div>
+    {[
+        'primary',
+        'secondary',
+        'success',
+        'danger',
+        'warning',
+        'info',
+        'light',
+        'dark',
+      ].map((variant) => (
+        <Alert key={variant} variant={variant}>
+          This is a {variant} alert—check it out!
+        </Alert>
+      ))}
+    </div> */}
+
+    </>
+  );
+}
+
+export default App;
---
+++ react-bootstrap/src/Sidebar.js
@@ -0,0 +1,14 @@
+import React from 'react'
+// import SidebarMenu from 'react-bootstrap-sidebar-menu';
+// import 'react-bootstrap-sidebar-menu/dist/sidebar-menu.scss'
+// import "../node_modules/react-bootstrap-sidebar-menu/dist/"
+
+
+function Sidebar() {
+  return (
+    <>
+    </>
+  )
+}
+
+export default Sidebar
